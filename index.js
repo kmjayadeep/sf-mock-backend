27d86f0d5522af018c1165a129c0b76ff73af9d0
Jayadeep KM <kmajayadeep@gmail.com>
2019-11-05 15:35:32 +0530

    s
    Signed-off-by: Jayadeep KM <kmajayadeep@gmail.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var fs = require('fs')
+var https = require('https')
+var app = express()
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/([\$])metadata', (req,res) => {
+  res.type('application/xml');
+  res.send(fs.readFileSync('metadata.xml'))
+})
+
+app.get('/User*', (req, res) => {
+  console.log(req.headers)
+  res.type('application/xml');
+  res.send(fs.readFileSync('User'));
+})
+
+https.createServer({
+  key: fs.readFileSync('security/keytmp.pem'),
+  cert: fs.readFileSync('security/cert.pem'),
+  requestCert: false,
+  rejectUnauthorized: false
+}, app)
+.listen(443, function () {
+  console.log('Example app listening')
+})
