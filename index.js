5ee175578faddf0f7f5c591056f0655b142d33df
Jayadeep KM <kmajayadeep@gmail.com>
2019-11-07 14:29:34 +0530

    lms items
    Signed-off-by: Jayadeep KM <kmajayadeep@gmail.com>

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var https = require('https')
 var app = express()
 const morgan = require('morgan');
 
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.get('/([\$])metadata', (req,res) => {
+app.get('/([\$])metadata', (req, res) => {
   res.type('application/xml');
   res.send(fs.readFileSync('metadata.xml'))
 })
@@ -20,12 +20,30 @@ app.get('/User*', (req, res) => {
   res.json(JSON.parse(user));
 })
 
-https.createServer({
-  key: fs.readFileSync('security/keytmp.pem'),
-  cert: fs.readFileSync('security/cert.pem'),
-  requestCert: false,
-  rejectUnauthorized: false
-}, app)
-.listen(443, function () {
-  console.log('Server started')
+app.post('/lms//oauth-api/rest/v1/token', (_, res) => {
+  res.json({
+    access_token: "token-sample",
+    expires_in: 1800,
+    token_type: "Bearer"
+  });
 })
+
+app.get('/lms//odatav4/public/admin/dataextraction-service/v1/Items', (req, res) => {
+  console.log(req.query);
+  if(req.query['$skip']==0){
+    const user = fs.readFileSync('Items.json');
+    res.json(JSON.parse(user));
+  }else {
+    res.json('ok');
+  }
+})
+
+https.createServer({
+    key: fs.readFileSync('security/keytmp.pem'),
+    cert: fs.readFileSync('security/cert.pem'),
+    requestCert: false,
+    rejectUnauthorized: false
+  }, app)
+  .listen(443, function () {
+    console.log('Server started')
+  })
