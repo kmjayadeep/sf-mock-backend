de494cd3581ec21a29e8977d82e355346a38e298
Jayadeep KM <kmjayadeep@gmail.com>
2019-11-08 12:21:31 +0530

    post apis
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var express = require('express')
-var fs = require('fs')
-var https = require('https')
-var app = express()
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const app = express();
 const moment = require('moment');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', function (_, res) {
   res.send('hello world')
 })
 
 app.get('/([\$])metadata', (req, res) => {
   res.type('application/xml');
   res.send(fs.readFileSync('metadata.xml'))
-})
-
-app.get('/User*', (req, res) => {
-  const user = fs.readFileSync('User.json');
-  res.json(JSON.parse(user));
 })
 
 app.post('/lms/oauth-api/rest/v1/token', (_, res) => {
@@ -36,13 +33,27 @@ const empty = {
   "value": []
 };
 
+let userData, itemData, offeringsData, participantsData;
+
+const resetData = () => {
+  userData = JSON.parse(fs.readFileSync('User.json'));
+  itemData = JSON.parse(fs.readFileSync('Items.json'));
+  offeringsData = JSON.parse(fs.readFileSync('Offerings.json'));
+  participantsData = JSON.parse(fs.readFileSync('OfferingParticipants.json'));
+}
+
+resetData();
+
+app.get('/User*', (req, res) => {
+  res.json(userData);
+})
+
 app.get('/lms/odatav4/public/admin/dataextraction-service/v1/Items', (req, res) => {
   res.setHeader('date', moment().format('ddd, DD MMM YYYY HH:mm:ss ') + 'GMT')
   // return res.json(empty);
   const filter = req.query['$filter'] || '';
   if (req.query['$skip'] == 0 && !filter.includes('FT_')) {
-    const user = fs.readFileSync('Items.json');
-    res.json(JSON.parse(user));
+    res.json(itemData);
   } else {
     res.json(empty);
   }
@@ -53,8 +64,7 @@ app.get('/lms/odatav4/public/admin/dataextraction-service/v1/ScheduledOfferings'
   // return res.json(empty);
   const filter = req.query['$filter'] || '';
   if (req.query['$skip'] == 0 && !filter.includes('FT_')) {
-    const user = fs.readFileSync('Offerings.json');
-    res.json(JSON.parse(user));
+    res.json(offeringsData);
   } else {
     res.json(empty);
   }
@@ -65,11 +75,51 @@ app.get('/lms/odatav4/public/admin/dataextraction-service/v1/ScheduledOfferingPa
   res.setHeader('date', moment().format('ddd, DD MMM YYYY HH:mm:ss ') + 'GMT')
   // return res.json(empty);
   if (req.query['$skip'] == 0 && !filter.includes('FT_')) {
-    const user = fs.readFileSync('OfferingParticipants.json');
-    res.json(JSON.parse(user));
+    res.json(participantsData);
   } else {
     res.json(empty);
   }
+})
+
+app.get('/data/:resource', (req, res)=>{
+  switch(req.params.resource){
+    case 'User':
+      return res.json(userData);
+    case 'Items':
+      return res.json(itemData);
+    case 'Offerings':
+      return res.json(offeringsData);
+    case 'OfferingParticipants':
+      return res.json(participantsData);
+  }
+  res.json('invalid resource');
+});
+
+app.post('/data/:resource', (req, res)=>{
+  const data = req.body;
+  console.log(data);
+  switch(req.params.resource){
+    case 'User':
+      userData = data;
+      break;
+    case 'Items':
+      itemData = data;
+      break;
+    case 'Offerings':
+      offeringsData = data;
+      break;
+    case 'OfferingParticipants':
+      participantsData = data;
+      break;
+    default:
+      res.json('invalid resource');
+  }
+  res.json('ok');
+});
+
+app.get('/reset', (_, res)=>{
+  resetData();
+  res.json('ok');
 })
 
 const env = process.env.NODE_ENV;
@@ -81,7 +131,7 @@ if (env == 'development') {
       requestCert: false,
       rejectUnauthorized: false
     }, app)
-    .listen(process.env.PORT || 3000, function () {
+    .listen(process.env.PORT || 443, function () {
       console.log('Server started')
     })
 } else {
